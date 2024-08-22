918bb2af0bd4e8820f38289a43fda498e8e22b35
KaBaCh143 <yurovskiy-dmitriy@mail.ru>
2024-08-22 13:01:25 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print((9 ** 0.5) * 5)  #1st program
+
+print(9.99>9.98 and 1000!= 1000.1)  #2nd program
+
+print(2*2+2)  #3rd program
+print(2*(2+2))
+print(6==8)
+
+print('123.456')  #4th program
+print(int((123.456*10)%10))
